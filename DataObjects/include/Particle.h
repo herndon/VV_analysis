0c75989ae8ac733dd1e7619f107f957f8146f026
Matthew Herndon <herndon@dyn-72-33-93-38.uwnet.wisc.edu>
2015-07-07 12:03:11 -0500

    Introduce Particle class with status and paratage information

---
+++ DataObjects/include/Particle.h
@@ -0,0 +1,41 @@
+#ifndef __PARTICLE_H__
+#define __PARTICLE_H__
+
+/**
+ * @file Particle.h
+ *
+ * @author Matt Herndon <herndon@hep.wisc.edu>
+ *
+ * @brief Stores the particle information for jet, letpon or vector boson.
+ *
+ * This class reads and stores all particle information from a single particle
+ * Includes particle type, TlorentzVector for basic kinematics, 
+ * and mother/daughter information.
+ */  
+
+
+#include "TLorentzVector.h"
+#include <vector>
+
+class Particle
+{
+private:
+    int _type;
+    TLorentzVector _lorentzVector;
+    std::vector<int> _mothers;
+    std::vector<int> _daughters;
+ public:
+    Particle(int _type, const TLorentzVector& lorentzVector, const std::vector<int>& mothers, const std::vector<int>& daughters);
+    int type() const {return _type;}
+    const TLorentzVector & lorentzVector() const {return _lorentzVector;}
+    const std::vector<int> & mothers() const {return _mothers;}
+    const std::vector<int> & daughters() const {return _daughters;}
+    bool isStable() const {return (_daughters.size() == 0);}
+    
+};
+
+
+
+#endif
+
+
---
+++ DataObjects/src/Particle.cpp
@@ -0,0 +1,21 @@
+/**
+ * @file PVector.h
+ *
+ * @author Matt Herndon <herndon@hep.wisc.edu>
+ *
+ * @brief Stores the particle information for jet, letpon or vector boson.
+ *
+ * This class reads and stores all particle information from a single particle
+ * Includes particle type, TlorentzVector for basic kinematics, 
+ * and mother/daughter information.
+ */  
+
+#include "DataObjects/include/Particle.h"
+
+Particle::Particle(int type, const TLorentzVector& lorentzVector, const std::vector<int>& mothers, const std::vector<int>& daughters):
+  _type(type),
+  _lorentzVector(lorentzVector),
+  _mothers(mothers),
+  _daughters(daughters)
+{
+}
